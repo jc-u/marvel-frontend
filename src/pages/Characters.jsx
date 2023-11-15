bc215bf2488892c98460fc6473fc4b0b6181f4b3
jc-u <jcurbain.admin@gmail.com>
2023-11-15 18:13:34 +0100

    Pagination added

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,27 +1,38 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Characters = () => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [totalPages, setTotalPages] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				const response = await axios.get(
-					"https://site--marvel-backend--tfb724g8njkw.code.run/marvel/characters/"
+					`https://site--marvel-backend--tfb724g8njkw.code.run/marvel/characters?limit=100&skip=${
+						(currentPage - 1) * 100
+					}`
 				);
+
 				setData(response.data.results);
+				setTotalPages(response.data.totalPages);
+
 				setIsLoading(false);
 			} catch (error) {
-				console.log("Error fetching characters:", error.message);
+				console.error("Error fetching characters:", error.message);
 				setIsLoading(false);
 			}
 		};
 
 		fetchData();
-	}, []);
+	}, [currentPage]);
+
+	const handlePageChange = (newPage) => {
+		setCurrentPage(newPage);
+	};
 
 	return isLoading ? (
 		<p>Loading...</p>
@@ -44,6 +55,19 @@ const Characters = () => {
 						</div>
 					</Link>
 				))}
+			</div>
+			<div className="pagination">
+				<button
+					onClick={() => handlePageChange(currentPage - 1)}
+					disabled={currentPage === 1}>
+					Previous
+				</button>
+				<span>{currentPage}</span>
+				<button
+					onClick={() => handlePageChange(currentPage + 1)}
+					disabled={currentPage === totalPages}>
+					Next
+				</button>
 			</div>
 		</div>
 	);
--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Comics = () => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [totalPages, setTotalPages] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const location = useLocation();
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				const response = await axios.get(
-					"https://site--marvel-backend--tfb724g8njkw.code.run/marvel/comics/"
+					`https://site--marvel-backend--tfb724g8njkw.code.run/marvel/comics?limit=100&skip=${
+						(currentPage - 1) * 10
+					}`
 				);
-
 				// Tri des comics par ordre alphabétique du titre
 				const sortedData = response.data.results.sort((a, b) =>
 					a.title.localeCompare(b.title)
 				);
 
 				setData(sortedData);
+				setTotalPages(response.data.totalPages);
 				setIsLoading(false);
 			} catch (error) {
 				console.log("Error fetching characters:", error.message);
@@ -27,8 +33,11 @@ const Comics = () => {
 		};
 
 		fetchData();
-	}, []);
+	}, [currentPage]);
 
+	const handlePageChange = (newPage) => {
+		setCurrentPage(newPage);
+	};
 	console.log(data);
 
 	return isLoading ? (
@@ -52,6 +61,20 @@ const Comics = () => {
 					</Link>
 				);
 			})}
+
+			<div className="pagination">
+				<button
+					onClick={() => handlePageChange(currentPage - 1)}
+					disabled={currentPage === 1}>
+					Previous
+				</button>
+				<span>{currentPage}</span>
+				<button
+					onClick={() => handlePageChange(currentPage + 1)}
+					disabled={currentPage === totalPages}>
+					Next
+				</button>
+			</div>
 		</div>
 	);
 };
